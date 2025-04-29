a7ec5c004a6f169e0c20438c2c830f6a252ba83d
maniket.kumar <maniket.kumar.32.3@mountblue.tech>
2025-04-29 11:50:58 +0530

    added user entity class
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/com/linkedin_clone_application/model/User.java
@@ -0,0 +1,57 @@
+package com.linkedin_clone_application.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String password;
+
+    @Column(length = 255)
+    private String headline;
+
+    @Column(length = 100)
+    private String location;
+
+    @Column(length = 1000)
+    private String about;
+
+    @Column(name = "profile_picture")
+    private String profilePicture;
+
+    @Column(name = "phone_number", length = 13)
+    private String phoneNumber;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}

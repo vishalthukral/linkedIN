890c62576fa81e61bac987a7b031a8241a30bb42
vishalthukral <vishalthukral5@gmail.com>
2025-04-30 16:59:46 +0530

    user controller modifed

--- src/main/java/com/linkedin_clone_application/controller/UserController.java
+++ src/main/java/com/linkedin_clone_application/controller/UserController.java
@@ -6,10 +6,7 @@ import com.linkedin_clone_application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
@@ -23,30 +20,37 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerPost(@ModelAttribute User user){
+    public String registerPost(@ModelAttribute User user) {
         userService.registerNewUser(user);
         return "redirect:/login";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard() {
         return "dashboard";
     }
 
     @GetMapping("/landingPage")
-    public String landingPage(){
+    public String landingPage() {
         return "landing_page";
+    }
+
+    @GetMapping("/create-profile/{Id}")
+    public String createProfile(@PathVariable int id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user",user);
+        return "profile";
     }
 }

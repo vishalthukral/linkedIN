a2ce0b535c375d176df1f364caf6385d9a202fc8
Chetan Bansal <chetan6053.be21@chitkara.edu.in>
2025-04-30 14:16:14 +0530

    PostController

---
+++ src/main/java/com/linkedin_clone_application/controller/PostController.java
@@ -0,0 +1,58 @@
+package com.linkedin_clone_application.controller;
+
+import com.linkedin_clone_application.model.Post;
+import com.linkedin_clone_application.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PostController {
+    private final PostService postService;
+    PostController(PostService postService){
+        this.postService = postService;
+    }
+
+    @GetMapping("/createpost")
+    public String createPost(){
+        return "createpost";
+    }
+    @PostMapping("/savepost")
+    public String savePost(@ModelAttribute Post post){
+        postService.savePost(post);
+        return "redirect:/";
+    }
+
+    @PostMapping("/deletepost")
+    @Transactional
+    public String deletePost(@RequestParam int id){
+        postService.deletePostById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String getAllPosts(Model model){
+        List<Post> postList = postService.getAllPost();
+        model.addAttribute("allposts",postList);
+        return "showallposts";
+    }
+
+    @GetMapping("/updateform")
+    @Transactional
+    public String updatePost(){
+        return "createpost";
+    }
+
+    @PostMapping("/updatepost")
+    @Transactional
+    public String updatePost(@ModelAttribute int id){
+        return "redirect:/";
+    }
+}

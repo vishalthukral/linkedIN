3a0ca5cfa799f6c8f93f32c2bf01f8470ad1276d
Chetan Bansal <chetan6053.be21@chitkara.edu.in>
2025-05-01 22:08:48 +0530

    jobs Repository

---
+++ src/main/java/com/linkedin_clone_application/repository/JobRepo.java
@@ -0,0 +1,19 @@
+package com.linkedin_clone_application.repository;
+
+import com.linkedin_clone_application.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepo extends JpaRepository<Job,Integer> {
+    // Fetch jobs by company
+//    List<Job> findByCompanyId(int companyId);
+
+    // Fetch jobs that match user’s interested job roles
+    List<Job> findByJobTitleIn(List<String> interestedRoles);
+
+    // Search jobs by tag or company role
+    List<Job> findByJobTitleContainingOrJobDescriptionContaining(String jobTitle, String jobDescription);
+}
